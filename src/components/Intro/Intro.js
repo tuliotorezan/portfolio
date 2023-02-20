3dbb03510f1f5092c98d536f9cc292de1c53fb65
Tulio Torezan <tuliotorezan@gmail.com>
2023-02-20 12:23:52 -0400

    Updating intro component

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -2,12 +2,12 @@ import React from 'react'
 
 function Intro(props) {
     return (
-        <section className="bg-gray-200 dark:bg-zinc-700">
+        <section className="bg-gray-200 dark:bg-zinc-800">
             <div className="max-w-6xl px-6 py-10 mx-auto">
                 <main className="relative z-20 w-full mt-8 md:flex md:items-center xl:mt-12">
-                    <div className="absolute w-full bg-main-color -z-10 md:h-96 rounded-2xl"></div>
-                    <div className="w-full p-6 bg-main-color md:flex md:items-center rounded-2xl md:bg-transparent md:p-0 lg:px-12 md:justify-evenly">
-                        <img className="h-2 4 w-24 solid md:mx-6 rounded-full object-cover shadow-md md:h-[32rem] md:w-80 lg:h-[22rem] lg:w-[26rem] md:rounded-2xl" src="/me.jpeg" alt="client photo" />
+                    <div className="absolute w-full bg-main-color -z-10 md:h-96"></div>
+                    <div className="w-full p-6 bg-main-color md:flex md:items-center md:bg-transparent md:p-0 lg:px-12 md:justify-evenly">
+                        <img className="h-2 4 w-24 md:mx-6 object-cover shadow-md md:h-[32rem] md:w-80 lg:h-[22rem] lg:w-[26rem] " src="/me.jpeg" alt="client photo" />
                         <div className="mt-2 md:mx-6">
                             <div>
                             <h1 className="mt-2 text-2xl font-semibold text-zinc-800 capitalize lg:text-3xl dark:text-gray-200">Hi I'm Túlio Torezan</h1>
