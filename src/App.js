6bd4401f4d6c75447e0b53f41fc02321cb3080cb
Tulio Torezan <tuliotorezan@gmail.com>
2023-02-20 12:23:36 -0400

    education component first draft

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Intro from './components/Intro/Intro';
+import Education from './components/Education/Education';
 
 function App() {
   return (
     <div className="App">
       <Intro/>
+      <Education/>
     </div>
   );
 }
