e8ee4442f6f969f1aabbbe6258ea5d699eaff0f5
Tulio Torezan <tuliotorezan@gmail.com>
2023-02-20 17:22:45 -0400

    work component initial draft

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Intro from './components/Intro/Intro';
 import Education from './components/Education/Education';
+import Work from './components/Work/Work';
 
 function App() {
   return (
     <div className="App">
       <Intro/>
+      <Work/>
       <Education/>
     </div>
   );
--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -5,7 +5,7 @@ function Intro(props) {
         <section className="bg-gray-200 dark:bg-zinc-800">
             <div className="max-w-6xl bg-main-color px-6 py-6 mx-auto">
                     <div className="w-full bg-main-color md:flex md:items-center md:bg-transparent md:p-0 lg:px-8 md:justify-evenly">
-                        <img className="h-2 4 w-24 object-cover shadow-md md:h-[32rem] md:w-80 lg:h-[22rem] lg:w-[26rem] " src="/me.jpeg" alt="client photo" />
+                        <img className="h-2 w-24 object-cover shadow-md md:h-[32rem] md:w-80 lg:h-[22rem] lg:w-[26rem] " src="/me.jpeg" alt="client photo" />
                         <div className="mt-2 md:ml-6">
                             <div>
                             <h1 className="mt-2 text-2xl font-semibold text-zinc-800 capitalize lg:text-3xl dark:text-gray-200">Hi I'm Túlio Torezan</h1>
